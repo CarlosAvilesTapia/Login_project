93ba092dd4c11bea4fb63676e49a0603805f152a
CAT <cavilest@gmail.com>
2023-06-20 01:18:12 -0400

    - Diseño completo. - Funciones y validaciones para ingresar datos.

--- app/src/main/java/com/example/loginproject/MainActivity.java
+++ app/src/main/java/com/example/loginproject/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.loginproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +14,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
 
-/*if(textName.getText().toString().isEmpty() || textLastName.getText().toString().isEmpty() || textEmail.getText().toString().isEmpty()
-        || textPassword.getText().toString().isEmpty() || !textEmail.getText().toString().contains("@") ){
-        Toast.makeText(getBaseContext(), "faltan campos por completar", Toast.LENGTH_SHORT).show();
-        return;
-        }*/
+        // Llamado al método creado para ingresar datos.
+        enterData();
+    }
+
+    private void enterData() {
+        // Creación de instancias generadas en el diseño.
+        EditText textName = findViewById(R.id.textName);
+        EditText textLastName = findViewById(R.id.textLastName);
+        EditText textEmail = findViewById(R.id.textEmail);
+        EditText textPassword = findViewById(R.id.textPassword);
+        Button createAccount = findViewById(R.id.createAccountButton);
+
+        // Creación de funcionalidad para el botón de crear cuenta.
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creación de variables para cada campo.
+                String name = textName.getText().toString().trim();
+                String lastName = textLastName.getText().toString().trim();
+                String email = textEmail.getText().toString();
+                String password = textPassword.getText().toString();
+
+                // Validación para el nombre.
+                if (name.isEmpty()) {
+                    Toast.makeText(getBaseContext(), "Ingrese su nombre por favor.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Validación para el apellido.
+                if (lastName.isEmpty()) {
+                    Toast.makeText(getBaseContext(), "Ingrese su apellido por favor.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Validación para el e-mail.
+                if (!email.contains("@") || !email.contains(".")) {
+                    Toast.makeText(getBaseContext(), "Ingrese un e-mail válido por favor.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Validación para el password.
+                if (password.isEmpty()) {
+                    Toast.makeText(getBaseContext(), "Ingrese una contraseña por favor.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+
+    }
+
+}
